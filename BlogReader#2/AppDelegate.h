ed389ca5ab0b4f5b7ff9ffe9712de646e3aa79a4
zoom <zoom@zoomtekiMac.local>
2014-04-24 09:40:08 -0700

    Initial Commit

---
+++ BlogReader#2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader#2
+//
+//  Created by zoom on 14-4-24.
+//  Copyright (c) 2014年 Jiance Tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader#2Tests/BlogReader_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  BlogReader_2Tests.m
+//  BlogReader#2Tests
+//
+//  Created by zoom on 14-4-24.
+//  Copyright (c) 2014年 Jiance Tong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlogReader_2Tests : XCTestCase
+
+@end
+
+@implementation BlogReader_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
